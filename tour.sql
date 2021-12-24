fef204cf24188aa872fcc060c13ab8a50f9198b5
ngoclam2001 <63953102+ngoclam2001@users.noreply.github.com>
2021-12-24 20:24:57 +0700

    Add files via upload

--- tour.sql
+++ tour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2021 at 01:25 PM
+-- Generation Time: Dec 24, 2021 at 02:23 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,9 +29,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `loaitour` (
   `MaLoai` int(11) NOT NULL,
-  `TenTour` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `id` char(10) COLLATE utf8mb4_unicode_ci NOT NULL
+  `TenTour` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `loaitour`
+--
+
+INSERT INTO `loaitour` (`MaLoai`, `TenTour`) VALUES
+(1, 'Tour trong nước'),
+(2, 'Tour nước ngoài');
 
 -- --------------------------------------------------------
 
@@ -41,8 +48,16 @@ CREATE TABLE `loaitour` (
 
 CREATE TABLE `nguoidung` (
   `TaiKhoan` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `MatKhau` char(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `MatKhau` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `binhluan` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `nguoidung`
+--
+
+INSERT INTO `nguoidung` (`TaiKhoan`, `MatKhau`, `binhluan`) VALUES
+('ngoclam', 'abcabc', NULL);
 
 -- --------------------------------------------------------
 
@@ -58,6 +73,13 @@ CREATE TABLE `thongtinchitiet` (
   `DiemDen` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `thongtinchitiet`
+--
+
+INSERT INTO `thongtinchitiet` (`ID`, `ThoiGianKhoiHanh`, `ThoiGianKetThuc`, `DiemKhoiHanh`, `DiemDen`) VALUES
+('001', '2021-12-15', '2021-12-21', 'Bà Nà', 'Hội An');
+
 -- --------------------------------------------------------
 
 --
@@ -71,8 +93,17 @@ CREATE TABLE `thongtinchung` (
   `DiaDiem` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `SoNgayDiTour` int(11) NOT NULL,
   `TraGop` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `KhuyenMai` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+  `KhuyenMai` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hinhanh` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LoaiTour` int(11) NOT NULL
 ) ;
+
+--
+-- Dumping data for table `thongtinchung`
+--
+
+INSERT INTO `thongtinchung` (`MaTour`, `ChuTour`, `TenTour`, `DiaDiem`, `SoNgayDiTour`, `TraGop`, `KhuyenMai`, `hinhanh`, `LoaiTour`) VALUES
+('001', 'Nguyễn Văn A', 'Đà Nẵng', 'Đà Nẵng', 10, 'không', 'có', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -82,8 +113,7 @@ CREATE TABLE `thongtinchung` (
 -- Indexes for table `loaitour`
 --
 ALTER TABLE `loaitour`
-  ADD PRIMARY KEY (`MaLoai`),
-  ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`MaLoai`);
 
 --
 -- Indexes for table `thongtinchitiet`
@@ -95,17 +125,12 @@ ALTER TABLE `thongtinchitiet`
 -- Indexes for table `thongtinchung`
 --
 ALTER TABLE `thongtinchung`
-  ADD PRIMARY KEY (`MaTour`);
+  ADD PRIMARY KEY (`MaTour`),
+  ADD KEY `LoaiTour` (`LoaiTour`);
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `loaitour`
---
-ALTER TABLE `loaitour`
-  ADD CONSTRAINT `loaitour_ibfk_1` FOREIGN KEY (`id`) REFERENCES `thongtinchitiet` (`ID`);
 
 --
 -- Constraints for table `thongtinchitiet`
@@ -113,6 +138,12 @@ ALTER TABLE `loaitour`
 ALTER TABLE `thongtinchitiet`
   ADD CONSTRAINT `thongtinchitiet_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `thongtinchung` (`MaTour`),
   ADD CONSTRAINT `thongtinchitiet_ibfk_2` FOREIGN KEY (`ID`) REFERENCES `thongtinchung` (`MaTour`);
+
+--
+-- Constraints for table `thongtinchung`
+--
+ALTER TABLE `thongtinchung`
+  ADD CONSTRAINT `thongtinchung_ibfk_1` FOREIGN KEY (`LoaiTour`) REFERENCES `loaitour` (`MaLoai`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
