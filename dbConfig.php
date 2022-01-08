f41ec01bacf9fbc02d59239cca0bcc4526d7391e
nguyenhuyhoang7511 <93707147+nguyenhuyhoang7511@users.noreply.github.com>
2022-01-08 13:22:40 +0700

    Add files via upload

--- dbConfig.php
+++ dbConfig.php
@@ -9,8 +9,9 @@ $dbName     = "hahalolo_tour";
 // $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-// KIỂM TRA CÓ LỖI HAY KHÔNG
+// Check connection/ KIỂM TRA CÓ LỖI HAY KHÔNG
 if ($db->connect_error) { // CÚ PHÁP -> DÙNG ĐỂ TRUY CẬP VÀO THUỘC TÍNH/ PHƯƠNG THỨC CỦA 1 ĐỐI TƯỢNG TRONG PHP
-   die("Connection failed: " . $db->connect_error);
+    die("Connection failed: " . $db->connect_error);
 }
 ?>
+
