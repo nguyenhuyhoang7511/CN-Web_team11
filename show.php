2e34c87e4bde58d15e856d40ce61a12464fb14d7
nguyenhuyhoang7511 <93707147+nguyenhuyhoang7511@users.noreply.github.com>
2022-01-08 13:26:42 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -3,7 +3,7 @@
 include 'dbConfig.php';
 
 // Get images from the database
-$query = $db->query("SELECT * FROM db_images ORDER BY uploaded_on DESC");
+$query = $db->query("SELECT * FROM db_images ORDER BY upload_on DESC");
 
 if($query->num_rows > 0){
     while($row = $query->fetch_assoc()){
