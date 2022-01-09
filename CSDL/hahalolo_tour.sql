b3185ebdd6e78e3be32628fe706c0320208ff961
ngoclam2001 <63953102+ngoclam2001@users.noreply.github.com>
2022-01-09 21:44:06 +0700

    Add files via upload

--- CSDL/hahalolo_tour.sql
+++ CSDL/hahalolo_tour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 31, 2021 at 04:45 PM
+-- Generation Time: Jan 09, 2022 at 03:43 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `db_comment` (
   `id` int(11) NOT NULL,
-  `user_comment` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `time_cmt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,8 +39,8 @@ CREATE TABLE `db_comment` (
 --
 
 INSERT INTO `db_comment` (`id`, `user_comment`, `message`, `time_cmt`) VALUES
-(32, '', 'Xin chào các bạn ', '2021-12-31 14:49:18'),
-(33, '', 'Xin chào các bạn 2', '2021-12-31 15:30:21');
+(32, 'Nguyễn Huy Hoàng', 'Xin chào các bạn ', '2022-01-09 14:35:04'),
+(33, 'Nguyễn Ngọc Lâm', 'Xin chào các bạn 2', '2022-01-09 14:35:17');
 
 -- --------------------------------------------------------
 
@@ -50,22 +50,11 @@ INSERT INTO `db_comment` (`id`, `user_comment`, `message`, `time_cmt`) VALUES
 
 CREATE TABLE `db_images` (
   `id` int(11) NOT NULL,
-  `file_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `uploaded_on` datetime NOT NULL,
-  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1',
-  `id_tour` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `db_images`
---
-
-INSERT INTO `db_images` (`id`, `file_name`, `uploaded_on`, `status`, `id_tour`) VALUES
-(8, 'dolan.PNG', '2021-12-30 14:40:02', '1', 1),
-(9, 'Bài tập lớn - Hệ quản trị CSDL.docx', '2021-12-30 15:15:00', '1', 1),
-(10, 'driver.PNG', '2021-12-30 15:15:55', '1', 2),
-(11, 'sdt.PNG', '2021-12-30 23:29:21', '1', 2),
-(12, 'MYSQL.PNG', '2021-12-30 23:29:33', '1', 3);
+  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uploaded_on` date NOT NULL,
+  `status` enum('1','0') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_tour` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -98,14 +87,14 @@ INSERT INTO `db_nguoidung` (`ma_nguoidung`, `ten_nguoidung`, `email`, `mat_khau`
 --
 
 CREATE TABLE `db_thongtinchitiet` (
-  `ID` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ThoiGianKhoiHanh` date NOT NULL,
   `ThoiGianKetThuc` date NOT NULL,
   `DiemKhoiHanh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `DiemDen` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `so_ngay` int(11) NOT NULL,
   `tra_gop` tinyint(1) NOT NULL,
-  `khuyen_mai` tinyint(1) NOT NULL
+  `khuyen_mai` tinyint(1) NOT NULL,
+  `id_tour` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -115,25 +104,23 @@ CREATE TABLE `db_thongtinchitiet` (
 --
 
 CREATE TABLE `db_thongtintour` (
-  `id_tour` int(11) NOT NULL,
-  `images` tinyblob NOT NULL,
-  `chu_tour` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_tour` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `chu_tour` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `loai_tour` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name_tour` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `dia_diem` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `thoi_gian` date NOT NULL,
+  `thoi_gian` datetime NOT NULL,
   `gia_tour` int(11) NOT NULL,
-  `mo_ta` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `mo_ta` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `db_thongtintour`
 --
 
-INSERT INTO `db_thongtintour` (`id_tour`, `images`, `chu_tour`, `loai_tour`, `name_tour`, `dia_diem`, `thoi_gian`, `gia_tour`, `mo_ta`) VALUES
-(1, '', 'Nguyễn Huy Hoàng', 'Tour miền bắc', '', '26 Sơn La', '2021-12-16', 3500000, '26 Sơn La chào ae cả nước'),
-(2, '', 'Nguyễn Ngọc Lâm', 'Tour miền bắc', '', 'Thành Phố Hà Nam', '2021-12-01', 1500000, 'Một trải nghiệm mới lạ tại Hà Nam'),
-(3, '', 'Lê Khắc Minh Đức', 'Tour Miền Bắc', '', 'Thành Phố Thanh Hóa', '2021-12-21', 4300000, 'Trải nghiệm thú vị ở biển Sầm Sơn');
+INSERT INTO `db_thongtintour` (`id_tour`, `chu_tour`, `loai_tour`, `name_tour`, `dia_diem`, `thoi_gian`, `gia_tour`, `mo_ta`) VALUES
+('HN01', 'Nguyễn Ngọc Lâm', 'Tour Miền Bắc', 'Hà Nam', 'Hà Nam', '2022-01-25 21:41:36', 1000000, 'rất vui'),
+('HN02', 'Lê Khắc Minh Đức', 'Tour Miền Bắc', 'Thanh Hóa', 'Thanh Hóa', '2022-02-25 21:41:36', 1110000, 'rất vui');
 
 --
 -- Indexes for dumped tables
@@ -143,7 +130,8 @@ INSERT INTO `db_thongtintour` (`id_tour`, `images`, `chu_tour`, `loai_tour`, `na
 -- Indexes for table `db_comment`
 --
 ALTER TABLE `db_comment`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_nguoidung` (`user_comment`);
 
 --
 -- Indexes for table `db_images`
@@ -163,14 +151,13 @@ ALTER TABLE `db_nguoidung`
 -- Indexes for table `db_thongtinchitiet`
 --
 ALTER TABLE `db_thongtinchitiet`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id_tour`);
 
 --
 -- Indexes for table `db_thongtintour`
 --
 ALTER TABLE `db_thongtintour`
-  ADD PRIMARY KEY (`id_tour`),
-  ADD UNIQUE KEY `only_name` (`chu_tour`);
+  ADD PRIMARY KEY (`id_tour`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -186,7 +173,7 @@ ALTER TABLE `db_comment`
 -- AUTO_INCREMENT for table `db_images`
 --
 ALTER TABLE `db_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `db_nguoidung`
@@ -199,6 +186,12 @@ ALTER TABLE `db_nguoidung`
 --
 
 --
+-- Constraints for table `db_comment`
+--
+ALTER TABLE `db_comment`
+  ADD CONSTRAINT `fk_nguoidung` FOREIGN KEY (`user_comment`) REFERENCES `db_nguoidung` (`ten_nguoidung`);
+
+--
 -- Constraints for table `db_images`
 --
 ALTER TABLE `db_images`
@@ -208,8 +201,7 @@ ALTER TABLE `db_images`
 -- Constraints for table `db_thongtinchitiet`
 --
 ALTER TABLE `db_thongtinchitiet`
-  ADD CONSTRAINT `db_thongtinchitiet_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `db_thongtinchung` (`MaTour`),
-  ADD CONSTRAINT `db_thongtinchitiet_ibfk_2` FOREIGN KEY (`ID`) REFERENCES `db_thongtinchung` (`MaTour`);
+  ADD CONSTRAINT `fk_thongtintour` FOREIGN KEY (`id_tour`) REFERENCES `db_thongtintour` (`id_tour`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
