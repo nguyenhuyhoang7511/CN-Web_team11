942261173908c282eb9f0bb2b6e2761b3eff3e94
lekhacminhduc <94516305+lekhacminhduc@users.noreply.github.com>
2022-01-11 20:51:45 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
     // TRƯỚC KHI CHO NGƯỜI DÙNG TRUY CẬP VÀO CẦN PHẢI KIỂM TRA (KỸ THUẬT SESSION)
     session_start();
 
-    if( isset($_SESSION['isLoginOK'])) // nếu không có tồn tại thì hủy phiên làm việc
+     if( isset($_SESSION['isLoginOK'])) // nếu không có tồn tại thì hủy phiên làm việc
     {
         unset($_SESSION['isLoginOK']); // Hủy phiên làm việc 
         header("location:index.php"); // chuyển hướng về index.php
