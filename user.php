f967003f9cacc30caf8578f225aaa59cc7eb2727
lekhacminhduc <94516305+lekhacminhduc@users.noreply.github.com>
2022-01-11 20:52:14 +0700

    Update user.php

--- user.php
+++ user.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['isLoginOK'])) // nếu không có tồn tại (không có 
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/logo_title/logo_title.png" type="image/x-icon" />
