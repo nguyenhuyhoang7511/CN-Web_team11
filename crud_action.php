e8feceb323e39e1ca50909cd57e4645ff5b8a5f1
nguyenhuyhoang7511 <93707147+nguyenhuyhoang7511@users.noreply.github.com>
2022-01-11 20:41:53 +0700

    Add files via upload

--- crud_action.php
+++ crud_action.php
@@ -6,8 +6,10 @@ $action = $_POST["action"];
 if(!empty($action)) {
 	switch($action) {
 		case "add":
+			// echo $_POST["USname"];
 		    $query = "INSERT INTO db_comment(message) VALUES('".$_POST["txtmessage"]."')";
 		    $insert_id = $db_handle->insert($query);
+			$d = date('Y-m-d H:i:s'); // Tạo ra 1 biến lưu thời gian
 		    if($insert_id){
 				  echo '<div class="message-box"  id="message_' . $insert_id . '">
 						<div>
@@ -19,15 +21,15 @@ if(!empty($action)) {
 						<div class="comment_user_content">
 							<div class="content_text">
 								<b>
-									Nguyễn Huy Hoàng
+								Nguyễn Huy Hoàng
 								</b>
 								<p><div class="message-content">' . $_POST["txtmessage"] . '</div></p>
 							</div>
 							<div class="btn_show_option">                                           
-								<button style="border: none; background-color: unset; " class=" fw-bold btnEditAction text-secondary  btn-sm" class="btnEditAction" name="edit" onClick="showEditBox(this,' . $insert_id . ')">Edit</button>
-								<button style="border: none; background-color: unset; " class=" fw-bold btnDeleteAction  text-secondary  btn-sm" class="btnDeleteAction" name="delete" onClick="callCrudAction(\'delete\',' . $insert_id . ')">Delete</button>
+								<button style="border: none; background-color: unset; " class=" fw-bold btnEditAction text-secondary  btn-sm" class="btnEditAction" name="edit" onClick="showEditBox(this,' . $insert_id . ')">Sửa</button>
+								<button style="border: none; background-color: unset; " class=" fw-bold btnDeleteAction  text-secondary  btn-sm" class="btnDeleteAction" name="delete" onClick="callCrudAction(\'delete\',' . $insert_id . ')">Xóa</button>
 								<button style="border: none; background-color: unset; " class=" fw-bold btnDeleteAction  text-secondary  btn-sm" >Haha</button>                                            
-								<span class="text-secondary">Thời gian : Đang cập nhật</span>
+								<span class="text-secondary">Thời gian : '.$d.'</span>
 							</div>						   
 						</div>
 					</div>												
@@ -52,4 +54,3 @@ if(!empty($action)) {
 	}
 }
 ?>
-
